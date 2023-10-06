997c7d7d16260cf6b7e955d205452f27575038db
Prathmesh Salve <116294312+PrathmeshSalve@users.noreply.github.com>
2023-10-06 15:37:27 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,62 @@
+"""
+LEVEL : B E G I N N E R.
+-----------------------------------------------------------
+Project - HangMan 𓍯🪢
+-----------------------------------------------------------
+Module:
+* random - random is a built-in module of the Python programming language.
+
+Functions Used:
+* Print - Prints a string into the console. || ex: print("Hello World").
+* Input - Prints a string into the console, and asks the user for a string input. || ex: name = input("What's your name").
+* len - it is use to know the length of the word or a sentence. || ex: len(chosen_word)
+
+Loops:
+* While loop 
+* For Loop
+
+--------------------------------------------------------------------------------------------------------------------------
+IMP Note : I have provided word list you can change it according to your work, Have funn.
+"""
+
+import random
+import hangman_words
+import hangman_art
+chosen_word = random.choice(hangman_words.word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+
+print(hangman_art.logo)
+
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    if guess in display:
+        print(f"you've already guessed {guess}.")
+
+    for position in range(word_length):
+        letter = chosen_word[position]
+
+        if letter == guess:
+            display[position] = letter
+
+    if guess not in chosen_word:
+        print(f"you guessed {guess}, that's not in the word. You lose a life.")
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+    print(f"{' '.join(display)}")
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    print(hangman_art.stages[lives])
